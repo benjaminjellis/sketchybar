8d3b1e59c0fe4232dcdeedf00a6bf856de27c463
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2025-01-03 22:38:07 +0000

    feat: add spaces

--- colors.sh
+++ colors.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PURPLE=0xFFc4a7e7
+export PURPLE=0xffebbcba
 export GREY=0x22908caa
 export TRANSPARENT=0x00000000
 export WHITE=0xFFe0def4
@@ -9,5 +9,5 @@ export MAGENTA=0xFFc4a7e7
 export ORANGE=0xFF966CFF
 export TEMPUS=0xFF6e6a86
 export STATUS=0xFF1f1d2e
-export SPACEBG=0xAAebbcba
+export SPACEBG=0xAAc4a7e7
 export MIDNIGHT=0xFF6e6a86
--- plugins/spaces.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source "$HOME/.config/sketchybar/colors.sh"
-source "$HOME/.config/sketchybar/icons.sh"
-source "$HOME/.config/sketchybar/environment.sh"
