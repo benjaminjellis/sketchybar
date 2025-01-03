5f3d3c5d67e0934b3b95f28724b91f9461188831
benjamin <32769527+benjaminjellis@users.noreply.github.com>
2025-01-03 20:59:58 +0000

    first commit

---
+++ colors.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export PURPLE=0xFFc4a7e7
+export GREY=0x22908caa
+export TRANSPARENT=0x00000000
+export WHITE=0xFFe0def4
+export BLUE=0x2931748f
+export MAGENTA=0xFFc4a7e7
+export ORANGE=0xFF966CFF
+export TEMPUS=0xFF6e6a86
+export STATUS=0xFF1f1d2e
+export SPACEBG=0xAAebbcba
+export MIDNIGHT=0xFF6e6a86
---
+++ plugins/spaces.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source "$HOME/.config/sketchybar/colors.sh"
+source "$HOME/.config/sketchybar/icons.sh"
+source "$HOME/.config/sketchybar/environment.sh"
